b5f3e5e427efd1c9830814a44b9f326e3eb5d950
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-26 21:08:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,10 @@ class Song
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name
+    artist.add_song(self)
+  end
 end
