1f939692dc2881ed89ede2c0bc59da5182cd0b16
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-26 21:07:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
-class Songs
-  attr_accessor :name
+class Song
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
 end
